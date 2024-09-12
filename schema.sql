3fdc9db860e7c8770632494c5bc09525fb273ee9
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-12 14:24:05 +0100

    encounters table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,25 @@
 CREATE TABLE conditions(
-START DATE,
-STOP DATE,
-PATIENT VARCHAR(1000),
-ENCOUNTER VARCHAR(1000),
-CODE VARCHAR(1000),
-DESCRIPTION VARCHAR(200),
+start DATE,
+stop DATE,
+patient VARCHAR(1000),
+encounter VARCHAR(1000),
+code VARCHAR(1000),
+description VARCHAR(200),
+);
+
+CREATE TABLE encounters(
+id VARCHAR(100),
+start TIMESTAMP,
+stop TIMESTAMP,
+patient VARCHAR(100),
+organisation VARCHAR(100),
+provider VARCHAR(100),
+payer VARCHAR(100),
+encounter_class VARCHAR(100),
+code VARCHAR(100),
+description VARCHAR(100),
+base_encounter_cost FLOAT,
+total_claim_cost FLOAT,
+payer_coverage FLOAT,
+reason_code VARCHAR(100)
 );
