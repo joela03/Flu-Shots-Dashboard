8fc3d2a2152f3669ee0b89f5cd88a18cc4031d9d
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-16 14:19:50 +0100

    joined select statements

--- queries.sql
+++ queries.sql
@@ -1,12 +1,12 @@
--- Select's active patient's (that have been to the hospital in the year 2022) that have received a flu-vaccine (code 5302)
-
-SELECT patients, MIN(date) AS earliest_flu_shot_2022
+-- Select's metrics from active patient's (that have been to the hospital in the year 2022) and have received a flu-vaccine (code 5302)
+with flu_shot_2022 AS
+(
+SELECT patient, MIN(date) AS earliest_flu_shot_2022
 FROM immunisations
 WHERE code = '5302'
 	AND date BETWEEN '2022-01-1 00:00' AND '2022-12-21 23:59'
 GROUP BY patient
-
--- Select various metrics from patient's mso that we can group by these metrics
+)
 SELECT pat.birthdate
 	  ,pat.race
 	  ,pat.county
@@ -14,3 +14,5 @@ SELECT pat.birthdate
 	  ,pat.first
 	  ,pat.last
 FROM patients AS pat
+LEFT JOIN flu_shot_2022 AS flu
+    on pat.id = flu.patient
