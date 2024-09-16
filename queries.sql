800a66534a880245cdf7335b3fb9406302930f64
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-16 14:40:53 +0100

    refactored select statement

--- queries.sql
+++ queries.sql
@@ -1,17 +1,17 @@
 -- Select's metrics from active patient's (that have been to the hospital in the year 2022) and have received a flu-vaccine (code 5302)
 
-with active_patients as
+WITH active_patients AS
 (
-SELECT patient,
+SELECT patient
 FROM encounters as e
 JOIN patients as pat
 ON e.patient = pat.id
 WHERE start BETWEEN '2022-01-1 00:00' AND '2022-12-31 23:59'
 AND pat.deathdate IS NULL
-and EXTRACT(MONTH FROM age('2022-12-31', pat.birthdate)) >= 6
+AND EXTRACT(MONTH FROM age('2022-12-31', pat.birthdate)) >= 6
 ),
 
-with flu_shot_2022 AS
+flu_shot_2022 AS
 (
 SELECT patient, MIN(date) AS earliest_flu_shot_2022
 FROM immunisations
