6f03eaf2fbbce0963ee8dc74911a851746b2d50e
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-16 11:56:14 +0100

    made load_data_to_db function

--- load_data.py
+++ load_data.py
@@ -11,3 +11,23 @@ def get_connection():
         port='5432'
     )
     return conn
+
+
+def load_data_to_db(table_name, data_file, conn):
+
+    df = pd.read_csv(data_file, delimiter=',')
+
+    curs = conn.cursor()
+
+    for i, row in df.iterrows():
+        cols = ', '.join(list(df.columns))
+        values = ', '.join(["%s"] * len(row))
+        insert_query = f"INSERT INTO {
+            table_name} ({cols}) VALUES ({values})"
+
+        curs.execute(insert_query, tuple(row))
+
+    conn.commit()
+    print(f"Data from {data_file} loaded into {table_name} successfully.")
+
+    curs.close()
