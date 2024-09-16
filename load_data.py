ca01c0010a429b39f6f8da7ed2e9ab11ff0ec482
Joel <trainee.joel.allen-caliste@sigmalabs.co.uk>
2024-09-16 11:52:35 +0100

    made get_connection function

--- load_data.py
+++ load_data.py
@@ -1,2 +1,13 @@
 import psycopg2
 import pandas as pd
+
+
+def get_connection():
+    """Sets up database connection"""
+    conn = psycopg2.connect(
+        dbname="healthcare",
+        user="joel",
+        host='localhost',
+        port='5432'
+    )
+    return conn
